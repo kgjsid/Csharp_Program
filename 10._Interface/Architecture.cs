3df5157d140dbd8278533eca96707a2059817a11
boun SEO <kgjsid@gmail.com>
2024-01-09 15:26:55 +0900

    docs : 인터페이스
    어떻게 상호작용할지 정의해 두는 인터페이스
    멤버를 가질 수 있지만 직접 구현하지 않으며 단지 정의만을 지님

---
+++ 10._Interface/Architecture.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.Interface
+{
+    class Architecture
+    {
+        // 추상클래스와 인터페이스
+
+        // 인터페이스는 추상클래스의 일종으로 특징이 동일함
+        // 함수에 대한 선언만 정의하고 이를 포함하는 클래스에서 구체화하여 사용
+        // 하지만, 추상클래스와 인터페이스를 통해 얻는 효과는 다르며 다른 역할을 수행함
+        // 개발자는 인터페이스와 추상클래스 중 더욱 상황에 적합한 것으로 구현해야 함
+
+        // <추상클래스와 인터페이스>
+        // 공통점 : 함수에 대한 선언만 정의하고 이를 포함하는 클래스에서 구체화하여 사용
+        // 차이점 : 추상클래스 - 변수, 함수의 구현 포함 가능 / 다중상속 불가
+        //         인터페이스 - 변수, 함수의 구현 포함 불가 / 다중포함 가능
+
+        // 추상클래스는 상속관계, 인터페이스는 행동을 포함하는 것
+
+        // 설계도
+        // 추상클래스는 (A Is B 관계)
+        // 상속 관계인 경우, 자식클래스가 부모클랫의 하위분류인 경우
+        // 상속을 통해 얻을 수 있는 효과를 얻을 수 있음
+        // 부모클래스의 기능을 통해 자식클래스의 기능을 확장하는 경우 사용
+
+        // 계약서
+        // 인터페이스 (A Can B 관계)
+        // 행동 포함인 경우, 클래스가 해당 행동을 할 수 있는 경우
+        // 인터페이스를 사용하는 모든 클래스와 상호작용이 가능한 효과를 얻을 수 있음
+        // 인터페이스에 정의된 함수들을 클래스의 목적에 맞게 기능을 구현하는 경우 사용
+
+        // ex) 건물 -> 은행 / 은행은 건물이다 => 상속
+        // ex) 들어갈 수 있다 -> 차 / 차는 들어갈 수 있다 => 인터페이스
+
+        public interface IEnterable
+        {
+            public void Enter();
+        }
+
+        public abstract class Building
+        {
+            protected int Position;
+            // 구현해야 할 함수를 제외하고 기능 함수
+            public abstract void Enter();
+        }
+
+        public class Bank : Building
+        {
+            // 만약 인터페이스를 사용하면 빌딩의 모든 기능을 다시 구현해야 함
+            // 상속을 받음으로써 구현해야 할 함수를 제외하고 전부 받을 수 있음
+            public override void Enter()
+            {
+
+            }
+        }
+
+        public class Car : IEnterable
+        {
+            // 부모의 기능이 자식에서 올바르지 않을 수도 있음 -> 상속 부적합
+            public void Enter()
+            {
+
+            }
+        }
+
+        /*
+        public abstract class EnterableObject
+        {
+            public string key;
+            public abstract void Enter();
+        }
+
+        public interface IEnterable
+        {
+            // public string key; -> 변수 정의 불가. 행동(함수)만 정의 가능
+            public void Enter();
+        }
+        */
+
+        // 역할자체는 매우 비슷 -> 반드시 포함하는 클래스에서 해당 기능을 구현해야 함
+        // 하지만 얻는 효과가 다르며 엄밀히 따지면 다른 역할을 수행함
+
+        // 서로가 각자 사용하기 좋은 상황이 있음
+
+        public class Human
+        {
+            public int hp;
+            public string name;
+        }
+
+        public interface IHitable
+        {
+            public void TakeHit(int damage);
+        }
+
+        public class Player : Human, IHitable
+        {
+            public Player()
+            {
+                hp = 50;
+            }
+
+            public void TakeHit(int damage)
+            {
+                hp -= damage;
+            }
+        }
+
+        public class NPC : Human
+        {
+            public NPC()
+            {
+                hp = 100;
+            }
+        }
+
+    }
+}
---
+++ 10._Interface/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace _10.Interface
+{
+    class Program
+    {
+        // 어떻게 상호작용할지 정의해 놓는 것
+        // UI? user interface -> 게임과 유저가 상호작용 할 수 있도록 도와주는 것
+        // interface? -> 무언가 상호작용 하기 위한 수단
+
+        // 인터페이스 (Interface)
+
+        // 인터페이스는 멤버를 가질 수 있지만 직접 구현하지 않음 단지 정의만을 가짐
+        // 인터페이스를 가지는 클래스에서 반드시 인터페이스의 정의를 구현해야 함
+        // 이를 반대로 표현하자면 인터페이스를 포함하는 클래스는
+        // 반드시 인터페이스의 구성 요소들을 구현했다는 것을 보장함
+        // Can-a 관계 : 클래스가 해당 행동을 할 수 있는 경우 적합함
+
+        // <인터페이스 정의>
+        // 일반적으로 인터페이스의 이름은 I로 시작함
+        // 인터페이스의 함수는 직접 구현하지 않고 정의만 진행
+        // 추상(abstract) 클래스와 유사
+        public interface IEnterable
+        {
+            public void Enter();
+        }
+
+        // <인터페이스 포함>
+        // 상속처럼 정의한 인터페이스를 클래스 : 붙여서 사용
+        // 인터페이스는 여러개 포함 가능
+        // 인터페이스를 포함하는 경우 반드시 인터페이스에서 정의한 함수를 구현해야만 함
+        public interface IOpenable
+        {
+            public void Open(); // 인터페이스를 포함한 클래스는 반드시 해당 함수가 존재
+        }
+        public class Box : IOpenable
+        {
+            public void Open()
+            {
+                Console.WriteLine("박스를 엽니다.");
+            }
+        }
+        public class Town : IEnterable
+        {
+            public void Enter()
+            {
+                Console.WriteLine("마을에 진입합니다.");
+            }
+        }
+
+        // 문은 열수도 있고 들어갈 수도 있으니 EnterObject도 상속 받아야 함.
+        // 하지만 상속은 하나만 가능
+        // public class Door : OpenObject, EnterObject -> 오류
+        // -> 이 때 인터페이스를 활용함.
+        public class Door : IOpenable, IEnterable // 인터페이스는 여러 내용을 사용할 수 있음
+        {
+            public void Open()
+            {
+                Console.WriteLine("문을 엽니다.");
+            }
+
+            public void Enter()
+            {
+                Console.WriteLine("문에 들어갑니다.");
+            }
+        }
+        public interface IDamagable
+        {
+            public void TakeHit(int damage);
+        }
+        // <인터페이스 사용>
+        // 인터페이스를 이용하여 기능을 구현할 경우
+        // 클래스의 상속관계와 무관하게 행동의 가능여부로 상호작용 가능
+        public class Player
+        {
+            public void Open(IOpenable openable)
+            {
+                Console.WriteLine("플레이어가 대상을 열기 시도합니다.");
+                openable.Open();
+            }
+
+            public void Enter(IEnterable enterable)
+            {
+                Console.WriteLine("플레이어가 대상에 들어가기 시도합니다.");
+                enterable.Enter();
+            }
+            public void Attack(IDamagable damagable)
+            {
+                Console.WriteLine("플레이어가 대상을 공격하기 시도합니다.");
+                damagable.TakeHit(10);
+            }
+        }
+
+        public class Monster : IDamagable
+        {
+            public int hp;
+
+            public void TakeHit(int damage)
+            {
+                hp -= damage;
+            }
+        }
+
+        // public class Orc : Monster, IEnterable 가능
+
+        static void Main(string[] args)
+        {
+            Player player = new Player();
+            Box box = new Box();
+            Town town = new Town();
+
+            player.Open(box);
+            player.Enter(town);
+
+            Door door = new Door();
+
+            player.Open(door);
+            player.Enter(door);
+        }
+
+        // 왜? 인터페이스를 사용하는지? -> 두번 이상의 상속으로 해결하면 될텐데? -> 죽음의 다이아몬드
+        // ex) 생명체 클래스, 육지 생명체 클래스, 해양 생명체 클래스, 개구리 클래스
+
+        // 생명체 : 혈액량
+        // 육지 생명체 : 숨쉬기() -> 허파, 혈액량
+        // 해양 생명체 : 숨쉬기() -> 아가미, 혈액량
+        // 개구리 클래스가 두개의 클래스를 상속받으면?
+        // -> 숨쉬기 기능을 어떻게 이용하는지?? -> 심지어 혈액량이 육지, 해양 각각 따로 있음
+
+        // 해당 클래스들의 상속 관계로 문제점 발생 -> 대신 인터페이스를 제공
+
+        // 그렇다면 상속을 전부 인터페이스로 대체하면?
+
+        // 추상 클래스와 인터페이스
+
+    }
+}
