464d1bba1470126d6abfcd81e1be72d6fa91df3c
boun SEO <kgjsid@gmail.com>
2024-01-09 15:44:54 +0900

    docs : 이벤트
    일련의 사건이 발생했다는 사실을 다른 객체에게 전달

---
+++ 15._Event/Architecture.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _15.Event
+{
+    class Architecture
+    {
+        public class Call
+        {
+            // <Call 방식>
+            // 일련의 사건이 발생한 순간에 대상의 함수를 호출하여 진행
+            // 장점 : 불필요한 연산 없이 일련의 사건이 발생한 타이밍에 처리 가능
+            // 단점 : 추가 기능 개발시 클래스를 수정해야하는 개방폐쇄의 원칙을 위반함
+
+            // -> 혼자가 아닌 여러명이 만들면? -> 만약 기능이 추가되면 player 클래스 내부의 소스를 변경해야 함
+            // 추가 개발시 마다 기존에 있던 Player의 소스가 변경이 필요해짐
+            // 게임은 항상 추가 기능이 필요해지고 그 때마다 기존의 소스가 영향을 받으면 클래스 수정이 필요함
+            // 클래스 내부 수정이 아닌 추가되는 기능만 수정하면?
+
+            // Polling? -> 계속 쳐다보고 있다
+            //  <Polling 방식>
+            // 대상에서 일련의 사건 발생을 확인하기 위해 계속해서 변경사항을 확인
+            // 장점 : 추가 기능 개발시에도 클래스를 수정하지 않아 개방폐쇄의 원칙을 준수함
+            // 단점 : 변경사항이 없는 경우에도 계속 확인해야하는 불필요한 연산이 발생
+
+            // 클래스 수정은 하지 않아도 괜찮음
+            // 다만 변경사항이 생겼을 때 마다 플레이어가 변경사항을 계속 전달하여야 함 -> UI가 지속해서 체크
+            // => 너무 불필요한 연산이 실행됨(변경되지 않아도 주기적으로 실행해야 하므로)
+
+            // <Event 방식>
+            // 일련의 사건이 발생하였을 때 반응할 대상을 참조하고 사건 발생시 호출하여 진행
+            // 장점 : 개방폐쇄의 원칙이 지켜지며, 불필요한 연산을 필요로 하지 않음
+            // 단점 : 이벤트를 구성하기 위한 추가적인 소스를 작성해야 하며 실시간 연산에는 매우 느림
+
+            // Event가 가장 좋은가???
+            // -> 위치와 같이 실시간 연산이 필요한 경우에는 Event방식이 느리며 Polling방식을 사용하는 것이 좋음 
+            // -> 보통 이벤트 처리는 호출에 있어 다른 방식보다 조금 느림
+
+            // => 세 방식의 장단점은 뚜렷하며 상황에 따라 올바른 방식을 이용할 수 있음
+            public class Player
+            {
+                public int hp = 100;
+
+                public UI ui;
+
+                public event Action OnChangeHp;
+
+                public void Hit(int damage)
+                {
+                    hp -= damage;
+                    Console.WriteLine($"Hit! Current Hp : {hp}");
+
+                    // 클래스에서 연관된 기능들을 직접 호출해야함
+                    // 만약 새로운 기능이 추가되는 경우 계속해서 수정될 부분
+                    ui.SetHp();
+
+                    if (OnChangeHp != null)
+                        OnChangeHp();
+                }
+            }
+
+            // <Call 방식>
+            public class UI
+            {
+                public void SetHp()
+                {
+                    Console.WriteLine("UI SetHp가 실행됨");
+                }
+            }
+
+            // <Polling 방식>
+            public class SFX
+            {
+                public Player player;
+                // SFX를 갱신하기 위해 주기적으로 실행해야 함
+                // 갱신이 늦을 경우 SFX에서 확인하는 내용이 실제 데이터와 다를 수 있음
+                // 계속 체크해야 함.
+                public void PlayerSound()
+                {
+                    // 플레이어를 계속 체크
+                }
+            }
+
+            // <Event 방식>
+            public class VFX
+            {
+                // 이벤트가 발생하였을 때 실행될 함수
+                // 다만 Main에서 해당 함수를 할당해주어야 함.
+                public void PlayerEffect()
+                {
+
+                }
+            }
+
+            static void Main()
+            {
+                Player player = new Player();
+                VFX vfx = new VFX();
+            }
+        }
+    }
+}
---
+++ 15._Event/Program.cs
@@ -0,0 +1,175 @@
+﻿namespace _15.Event
+{
+    class Program
+    {
+        // 이벤트(Event)
+
+        // 일련의 사건이 발생했다는 사실을 다른 객체에게 전달
+        // 델리게이트의 일부 기능을 제한하여 이벤트의 용도로 사용
+
+        // A <-> B 두 객체는 Call, Callback 방식으로 구성
+        // + A객체가 모든 객체에게 전파의 목적으로 구성하는 방식? 
+
+        // ex) Player가 데미지 받음(이벤트) -> 모든 객체에게 플레이어가 맞았음을 알려줌
+        // -> 필요로 하는 객체가 있을 것(체력바, 사운드, 이펙터, 게임 매니저, ...) -> 그리고 해당 객체는 각자 다른 일을 수행함
+        // 이 내용이 이벤트의 컨셉 : 여러 객체들에게 처리를 전달하고 대응하도록 하는 것
+        // => 콜백을 여러 대상한테 전달
+
+        // <이벤트 선언>
+        // 델리게이트 변수 앞에 event 키워드를 추가하여 이벤트로 선언
+        public class Player
+        {
+            private int hp;
+
+            public event Action OnGetCoin;    // 이벤트 -> 코인을 먹었을 때 발생시킬 이벤트
+            public Action OnDamaged;          // 델리게이트 -> 데미지 받았을 때 발생시킬 델리게이트
+            public Action OnDied;
+
+            private void Die()
+            {
+                if (OnDied != null)
+                {
+                    OnDied();
+                }
+            }
+
+            public void GetCoin()
+            {
+                Console.WriteLine("플레이어가 코인을 얻음");
+
+                if (OnGetCoin != null)
+                {
+                    OnGetCoin();       // 일련의 사건이 발생했을 때 이벤트 발생
+                }
+            }
+
+            public void GetDamaged()
+            {
+                Console.WriteLine("플레이어가 데미지를 받음");
+
+                if (OnDamaged != null)
+                {
+                    OnDamaged();       // 일련의 사건이 발생했을 때 이벤트 발생
+                }
+            }
+        }
+
+        // <이벤트 등록 & 해제>
+        // 이벤트에 반응할 객체의 추가할 함수를 += 연산자를 통해 참조 추가
+        // 이벤트에 반응할 객체의 제거할 함수를 -= 연산자를 통해 참조 제거
+        static void Main5(string[] args)
+        {
+            Player player = new Player();
+            UI ui = new UI();
+            SFX sfx = new SFX();
+            VFX vfx = new VFX();
+
+            // 이벤트에 반응할 객체의 함수 추가
+            player.OnGetCoin += ui.CoinUI;      // 플레이어의 OnGetCoin에 CoinUI 함수 추가
+            player.OnGetCoin += sfx.CoinSound;  // 플레이어의 OnGetCoin에 CoinSound 함수 추가
+            player.OnGetCoin += vfx.CoinEffect; // 플레이어의 OnGetCoin에 CoinEffect 함수 추가
+
+            player.GetCoin();
+            // 플레이어가 코인을 얻는 함수를 실행시키며 이벤트를 발생시키고,
+            // OnGetCoin에 참조되어 있던 모든 함수를 실행시킴
+            // ui.CoinUI();
+            // sfx.CoinSound();
+            // vfx.CoinEffect(); => 함수들을 호출시켜야 하는 상황을 이벤트 처리로 한번에 호출 
+
+            // 이후로 언제든지 GetCoin을 실행시키면 모든 함수가 동시에 반응함
+            // 또한 만약 추가해야 한다면? 
+            // player.OnGetCoin += 추가할 함수 로 쉽게 수정 가능
+            // player.OnGetCoin -= sfx.CoinSound;
+
+            // <문제점>
+            // 1. 델리게이트 체인 = (대입) 가능
+            // player.OnGetCoin = sfx.CoinSound;
+            // (=) 대입은 기존의 객체를 전부 사라지게 만드니 꼭 주의해야 할 부분
+            // 델리게이트로 이벤트를 구현할 때 문제사항 -> 막아야 하는 부분
+
+            // 2. 이벤트 발생이 외부에서도 가능
+            // player.OnGetCoin();
+            // 외부에서도 호출 가능 -> 플레이어가 특정 상황이 아닌데도 갑자기 실행될 수도 있음
+            // player.OnDied();
+            // 이벤트 발생을 외부에서 막을 수 없음
+
+            // 해결 방법은?? 델리게이트 변수 앞에 event 키워드 활용
+            // 이벤트와 델리게이트의 차이?
+            // 1. 이벤트는 (=) 대입이 불가능, 할당과 제거만 가능
+            // 2. 클래스 외부에서 이벤트를 발생시킬 수 없음
+        }
+
+        public class UI
+        {
+            public void CoinUI() { Console.WriteLine("UI에 코인수를 갱신"); }
+        }
+
+        public class SFX
+        {
+            public void CoinSound() { Console.WriteLine("코인을 얻는 소리 재생"); }
+        }
+
+        public class VFX
+        {
+            public void CoinEffect() { Console.WriteLine("코인을 얻는 이펙트 재생"); }
+        }
+
+
+        // <델리게이트 체인과 이벤트의 차이점>
+        // 델리게이트 또한 체인을 통하여 이벤트로서 구현이 가능
+        // 하지만 델리게이트는 두가지 사항 때문에 이벤트로서 사용하기 적합하지 않음
+        // 1. = 대입연산을 통해 기존의 이벤트에 반응할 객체 상황이 초기화 될 수 있음
+        // 2. 클래스 외부에서 이벤트를 발생시켜 원하지 않는 상황에서 이벤트 발생 가능
+        // event 키워드를 추가할 경우 델리게이트에서 위 두가지 기능을 제한하여 이벤트 전용으로 사용을 유도할 수 있음
+        // 즉, event 변수는 델리게이트에서 기능을 제한하여 이벤트 전용의 기능만으로 사용하는 기능
+
+        public class EventSender
+        {
+            public event Action OnEvent;
+            public Action OnDelegate;
+
+            public void DelegateCall()
+            {
+                if (OnDelegate != null)
+                {
+                    OnDelegate();
+                }
+            }
+            public void EventCall()
+            {
+                if (OnEvent != null)
+                {
+                    OnEvent();
+                }
+            }
+
+        }
+
+        public class EventListener
+        {
+            public void ReAction() { }
+        }
+
+        void Main2()
+        {
+            EventSender sender = new EventSender();
+            EventListener listener1 = new EventListener();
+            EventListener listener2 = new EventListener();
+            EventListener listener3 = new EventListener();
+
+            // 델리게이트는 대입연산이 가능하며 이벤트에 반응할 객체들의 상황을 잃을 문제점
+            sender.OnDelegate += listener1.ReAction;
+            sender.OnDelegate += listener2.ReAction;
+            sender.OnDelegate = listener3.ReAction; // 주의! 기존의 이벤트에 반응할 객체들이 사라짐
+
+            // 이벤트는 대입연산이 불가능하며 할당 및 제거만 가능
+            sender.OnEvent += listener1.ReAction;
+            sender.OnEvent += listener2.ReAction;
+            // sender.OnEvent = listener3.ReAction; // error : 이벤트는 (=) 연산자 사용 불가능
+
+            sender.OnDelegate(); // 심지어 외부에서 이벤트를 강제로 실행시킬 수 있음
+            // sender.OnEvent(); // 이벤트는 불가능 반드시 특정 함수를 통해 실행시킴
+            sender.EventCall();
+        }
+    }
+}
